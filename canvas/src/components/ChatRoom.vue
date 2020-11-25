a2826d293a3c5b697560878067516b7894146ca6
Anderson <MDAnderson@samaritan.org>
2020-11-25 15:46:21 -0500

    na

--- canvas/src/components/ChatRoom.vue
+++ canvas/src/components/ChatRoom.vue
@@ -1,20 +1,100 @@
 <template>
-  <main class="outline">
-    <h3>ChatRoom.vue {{ chatId }}</h3>
+  <main class="section">
+    <h3>Welcome to ChatRoom.vue {{ chatId }}</h3>
 
     <router-link to="/">Back</router-link>
 
+    <p>
+    Open this link in another browser window to chat 
+      <code>url/chats/{{ chatId }}</code>
+    </p>
+
+    <User #user="{ user }">
+      <div v-if="user">
+        <ul>
+          <li v-for="message of messages" :key="message.id">
+            <ChatMessage :message="message" :owner="user.uid === message.sender" />
+          </li>
+        </ul>
+
+        <input v-model="newMessageText" class="input" />
+
+        <button
+          :disabled="(!newMessageText) || loading"
+          class="button is-success"
+          type="text"
+          @click="addMessage(user.uid)"
+        >Send</button>
+      </div>
+
+      <Login v-else />
+    </User>
   </main>
 </template>
 
 <script>
-
+import User from './User.vue';
+import ChatMessage from './ChatMessage.vue';
+import Login from './Login.vue';
+import { db } from '../firebase';
 export default {
+  components: {
+    User,
+    Login,
+    ChatMessage,
+  },
+  data() {
+      return {
+          newMessageText: '',
+          loading: false,
+          messages: [],
+      }
+  },
   computed: {
     chatId() {
       return this.$route.params.id;
     },
+    messagesCollection() {
+      return db.doc(`chats/${this.chatId}`).collection('messages');
+    },
   },
+  firestore() {
+    return {
+      messages: this.messagesCollection.orderBy('createdAt').limitToLast(10)
+    };
+  },
+  methods: {
+    async addMessage(uid) {
+        this.loading = true;
+        const { id: messageId } = this.messagesCollection.doc();
 
+         await this.messagesCollection.doc(messageId).set({
+            text: this.newMessageText,
+            sender: uid,
+            createdAt: Date.now(),
+        });
+        this.loading = false;
+        this.newMessageText = '';
+        this.newAudio = null;
+    },
+  }
 };
 </script>
+
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
+  background: #efefef;
+  padding: 10px;
+  border-radius: 0;
+}
+li {
+  display: flex;
+}
+</style>
