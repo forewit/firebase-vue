94a236f88b72552b77c7679fc94ebb43ff64adc7
Anderson <MDAnderson@samaritan.org>
2020-11-25 15:18:09 -0500

    na

--- canvas/src/components/Home.vue
+++ canvas/src/components/Home.vue
@@ -6,6 +6,7 @@
     <User v-slot:user="{ user }">
       <div v-if="user">
         <UserProfile :user="user" />
+        <ChatList :uid="user.uid" />
       </div>
       <Login v-else />
     </User>
@@ -16,12 +17,14 @@
 import Login from './Login'
 import User from './User'
 import UserProfile from './UserProfile'
+import ChatList from './ChatList'
 
 export default {
     components: {
         Login, 
         User,
         UserProfile,
+        ChatList,
     }
 }
 </script>
